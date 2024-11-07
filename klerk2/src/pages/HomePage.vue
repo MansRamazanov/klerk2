50dc4aa3a0a74d25b3f9ef3e13a63089cdba10cd
Mans <m.ramazanov@wi-fi.click>
2024-11-07 23:47:48 +0300

    initial commit

---
+++ klerk2/src/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="header">
+            <img src="../assets/logo.svg" alt="Логотип Клерк">
+            <div class="breadcrumbs">
+                <router-link to="/">Хлебные крошки</router-link>
+                <router-link to="/">Хлебные крошки</router-link>
+                <router-link to="/">Хлебные крошки</router-link>
+            </div>
+            
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+    
+</style>
